8d62dd1a2d2386370e640a37dcda3094d65a9234
sa9a7 <100088077+sa9a7@users.noreply.github.com>
2022-06-28 22:56:26 +0545

    Update guesser.py

--- guesser.py
+++ guesser.py
@@ -6,7 +6,6 @@ h=int(input("Enter the upper limit"))
 inp=''
 while inp!='c':
 	arr.append(r)
-	#print(arr) #debug line
 	r=random.randint(l,h)
 	if r not in arr:
 		inp=input(f"Is {r} the right answer? (l,h or c) ")
